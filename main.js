603e98012c1f59c14395f3030b445bf7404b0e1a
Lesya <vlasenko.lesja1@gmail.com>
2023-04-21 20:33:35 +0200

    #2113  add file

---
+++ main.js
@@ -0,0 +1,8 @@
+/*const cowsayjs = require ("cowsayjs");
+console.log(cowsayjs.moo("can you see me?"));*/
+const CC = require('currency-converter-lt');
+let currencyConverter = new CC({from:"USD", to:"EUR", amount:100});
+currencyConverter.convert().then((response)=>{
+    console.log(response)
+})
+console.log(`Result: ${currencyConverter}`);
