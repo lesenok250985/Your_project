d959273e5fa8b076b5cf466ba23611d6d41c8b8f
Lesya <vlasenko.lesja1@gmail.com>
2023-11-06 18:12:59 +0100

    task#2 add new information

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-/*const cowsayjs = require ("cowsayjs");
-console.log(cowsayjs.moo("can you see me?"));*/
+const cowsayjs = require ("cowsayjs");
+console.log(cowsayjs.moo("can you see me?"));
 const CC = require('currency-converter-lt');
 let currencyConverter = new CC({from:"USD", to:"EUR", amount:100});
 currencyConverter.convert().then((response)=>{
     console.log(response)
 })
-console.log(`Result: ${currencyConverter}`);
+console.log(`Result: ${currencyConverte}`);
+
+const cowsayjs = require ("cowsayjs");
+console.log(cowsayjs.moo("can you see me?"));
